d8e66483d772587a9241084a3513bae71d52c7d8
Chase Poirier <chase.n.poirier@gmail.com>
2018-10-19 13:46:05 -0400

    added force ssl on production

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
 import express from 'express'
 import path from 'path'
-import favicon from 'serve-favicon'
+// import favicon from 'serve-favicon'
 import logger from 'morgan'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-import forceDomain from 'forcedomain'
 
 import index from './routes/index'
 import admin from './routes/admin'
@@ -16,19 +15,14 @@ const app = express()
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// if (process.env.NODE_ENV !== 'dev') {
-//   app.use(function(req, res, next) {
-//     if (req.headers['x-forwarded-proto'] !== 'https') {
-//       return res.redirect(301, ['https://', req.get('Host'), req.url].join(''))
-//     }
-//     return next()
-//   })
-// app.use(forceDomain({
-//     hostname: 'chasepoirier.com',
-//     protocol: 'https'
-// }));
-// app.use(forceSsl);
-// }
+if (process.env.NODE_ENV !== 'dev') {
+  app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(301, ['https://', req.get('Host'), req.url].join(''))
+    }
+    return next()
+  })
+}
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
@@ -42,8 +36,6 @@ app.use('/api/admin', admin)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
 })
-
-// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
