013fd95ec5427bf098e4219ebbd16347e1b037a6
Chase Poirier <chase@chasepoirier.com>
2024-12-06 11:52:52 -0700

    chore: page cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import AllFeatures from "@/components/AllFeatures";
-import CTA from "@/components/CTA";
-import FAQ from "@/components/FAQ";
-import FeatureCallout from "@/components/FeatureCallout";
+// import AllFeatures from "@/components/AllFeatures";
+// import CTA from "@/components/CTA";
+// import FAQ from "@/components/FAQ";
+// import FeatureCallout from "@/components/FeatureCallout";
 import Footer from "@/components/Footer";
 import Hero from "@/components/home/hero";
-import LogoSlider from "@/components/LogoSlider";
+// import LogoSlider from "@/components/LogoSlider";
 import Navbar from "@/components/Navbar";
-import StatStepper from "@/components/StatStepper";
-import Timeline from "@/components/Timeline";
+// import StatStepper from "@/components/StatStepper";
+// import Timeline from "@/components/Timeline";
 import { createSession } from "@/lib/stripe";
 
 export default async function Home() {
@@ -17,13 +17,13 @@ export default async function Home() {
     <>
       <Navbar />
       <Hero />
-      <LogoSlider />
+      {/* <LogoSlider />
       <StatStepper />
       <FeatureCallout />
       <AllFeatures />
       <Timeline />
       <FAQ />;
-      <CTA stripeCheckoutUrl={stripeCheckoutUrl} />
+      <CTA stripeCheckoutUrl={stripeCheckoutUrl} /> */}
       <Footer />
     </>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ interface Item {
 const navigation: Item[] = [
   {
     name: "Instagram",
-    href: "https://instagram.com/",
+    href: "https://instagram.com/chase_codes",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
@@ -20,7 +20,7 @@ const navigation: Item[] = [
   },
   {
     name: "X",
-    href: "https://x.com/",
+    href: "https://x.com/chase_codes",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
@@ -29,7 +29,7 @@ const navigation: Item[] = [
   },
   {
     name: "GitHub",
-    href: "https://github.com/",
+    href: "https://github.com/chasepoirier",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
@@ -42,7 +42,7 @@ const navigation: Item[] = [
   },
   {
     name: "YouTube",
-    href: "https://youtube.com/@",
+    href: "https://youtube.com/@chase_codes",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
@@ -74,8 +74,8 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; {new Date().getFullYear()} Lemonade Tech. All rights
-            reserved.
+            &copy; {new Date().getFullYear()} Poirier Design & Development, LLC.
+            All rights reserved.
           </p>
         </div>
       </div>
